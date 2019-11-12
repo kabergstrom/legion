68d516e5eac35e37ff53d99397c48b634e54f292
Karl Bergström <karl.anton.bergstrom@gmail.com>
2019-11-12 15:30:56 +0100

    Progress

--- src/storage.rs
+++ src/storage.rs
@@ -234,7 +234,7 @@ impl Storage {
 }
 
 /// Stores metadata decribing the type of a tag.
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq)]
 pub struct TagMeta {
     size: usize,
     align: usize,
@@ -270,7 +270,7 @@ impl TagMeta {
 }
 
 /// Stores metadata describing the type of a component.
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq)]
 pub struct ComponentMeta {
     size: usize,
     align: usize,
@@ -290,7 +290,7 @@ impl ComponentMeta {
 
 /// Describes the layout of an archetype, including what components
 /// and tags shall be attached to entities stored within an archetype.
-#[derive(Default, Clone)]
+#[derive(Default, Clone, PartialEq)]
 pub struct ArchetypeDescription {
     tags: Vec<(TagTypeId, TagMeta)>,
     components: Vec<(ComponentTypeId, ComponentMeta)>,
